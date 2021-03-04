c1b1d48d478e618101f682f7e64c2e9a2528065e
kas054 <kadurova.anastasia2002@yandex.ru>
2021-03-04 22:35:16 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/lab0_5_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab0_5_test.dir/main.c.obj"
+  "lab0_5_test.exe"
+  "lab0_5_test.exe.manifest"
+  "lab0_5_test.pdb"
+  "liblab0_5_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab0_5_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
